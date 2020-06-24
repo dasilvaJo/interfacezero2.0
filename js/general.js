2df77b32c2a4b2ddcf3a6b8c4f6bd00e3fd8a72d
dasilvaJo <66748989+dasilvaJo@users.noreply.github.com>
2020-06-24 15:59:47 +0200

    Version 1

---
+++ js/general.js
@@ -0,0 +1,151 @@
+function transitionIn() {
+  document.getElementById("backgroundLeft").style["animation"] =
+    "2s ease leftToRight both";
+  document.getElementById("backgroundRight").style["animation"] =
+    "2s ease rightToLeft both";
+  document.getElementById("backgroundLeft2").style["animation"] =
+    "2s ease leftToRight both";
+  document.getElementById("backgroundRight2").style["animation"] =
+    "2s ease rightToLeft both";
+  document.getElementById("logo").style["animation"] =
+    "2s ease leftToRight both";
+  document.getElementById("fleche").style["animation"] =
+    "2s ease rightToLeft both";
+  document.getElementById("logo").style["animation-delay"] = "1s";
+  document.getElementById("fleche").style["animation-delay"] = "1s";
+}
+
+function transitionOut(value) {
+  document.getElementById("logo").style["animation"] =
+    "2s ease leftToRight2 both";
+  document.getElementById("fleche").style["animation"] =
+    "2s ease rightToLeft2 both";
+  if (value == 1) {
+    document.getElementById("backgroundLeft").style["animation"] =
+      "2s ease leftToRight2 both";
+    document.getElementById("backgroundRight").style["animation"] =
+      "2s ease rightToLeft2 both";
+    document.getElementById("backgroundLeft2").style["animation"] =
+      "2s ease leftToRight2 both";
+    document.getElementById("backgroundRight2").style["animation"] =
+      "2s ease rightToLeft2 both";
+    document.getElementById("backgroundLeft").style["animation-delay"] = "1s";
+    document.getElementById("backgroundRight").style["animation-delay"] = "1s";
+    document.getElementById("backgroundLeft2").style["animation-delay"] = "1s";
+    document.getElementById("backgroundRight2").style["animation-delay"] = "1s";
+    document.getElementById("carte").style["animation"] =
+      "1s ease scaleIn both";
+    document.getElementById("carte").style["animation-delay"] = "2s";
+  } else if (value == 2) {
+    document.getElementById("menu1").style["animation"] =
+      "2s ease rightToLeft2 both";
+    document.getElementById("menu2").style["animation"] =
+      "2s ease rightToLeft2 both";
+    document.getElementById("backgroundLeft").style["animation"] =
+      "1s ease changeHeight both";
+    document.getElementById("backgroundRight").style["animation"] =
+      "1s ease changeHeight both";
+    document.getElementById("backgroundLeft").style["animation-delay"] = "2s";
+    document.getElementById("backgroundRight").style["animation-delay"] = "2s";
+    document.getElementById("write").style["animation"] =
+      "1s ease scaleIn both";
+    document.getElementById("write").style["animation-delay"] = "2.5s";
+    document.getElementById("title1").style["animation"] =
+      "1s ease scaleIn both";
+    document.getElementById("title1").style["animation-delay"] = "2.5s";
+    document.getElementById("writeSave").style["animation"] =
+      "1s ease scaleIn both";
+    document.getElementById("writeSave").style["animation-delay"] = "2.5s";
+    document.getElementById("title2").style["animation"] =
+      "1s ease scaleIn both";
+    document.getElementById("title2").style["animation-delay"] = "2.5s";
+    document.getElementById("previsual").style["animation"] =
+      "1s ease scaleIn both";
+    document.getElementById("previsual").style["animation-delay"] = "2.5s";
+    document.getElementById("title3").style["animation"] =
+      "1s ease scaleIn both";
+    document.getElementById("title3").style["animation-delay"] = "2.5s";
+  }
+}
+
+function playSound() {
+  var audio = document.getElementById("sound");
+  audio.play();
+}
+
+function stopSound() {
+  var audio = document.getElementById("sound");
+  audio.pause();
+  audio.currentTime = 0;
+}
+
+var text = "";
+var res = "";
+var i = 0;
+function display() {
+  text = document.getElementById("write").value;
+  res = "";
+  i = 0;
+  display2();
+}
+
+function display2() {
+  setTimeout(function () {
+    res = res + text.charAt(i);
+    document.getElementById("previsual").value = res;
+    i = i + 1;
+    if (i < text.length) {
+      display2();
+    }
+  }, 300);
+}
+
+function display3() {
+  text = document.getElementById("write").value;
+  res = "";
+  i = 0;
+  display4();
+}
+
+function display4() {
+  setTimeout(function () {
+    res = res + text.charAt(i);
+    document.getElementById("displayText").innerHTML = res;
+    i = i + 1;
+    if (i < text.length) {
+      display4();
+    }
+  }, 500);
+}
+
+function textAnim() {
+  document.getElementById("write").style["animation"] = "1s ease scaleOut both";
+  document.getElementById("title1").style["animation"] =
+    "1s ease scaleOut both";
+  document.getElementById("title2").style["animation"] =
+    "1s ease scaleOut both";
+  document.getElementById("title3").style["animation"] =
+    "1s ease scaleOut both";
+  document.getElementById("previsual").style["animation"] =
+    "1s ease scaleOut both";
+  document.getElementById("writeSave").style["animation"] =
+    "1s ease scaleOut both";
+  document.getElementById("backgroundLeft").style["height"] = "90%";
+  document.getElementById("backgroundRight").style["height"] = "90%";
+  document.getElementById("backgroundLeft").style["animation"] =
+    "2s ease leftToRight2 both";
+  document.getElementById("backgroundRight").style["animation"] =
+    "2s ease rightToLeft2 both";
+  document.getElementById("backgroundLeft2").style["animation"] =
+    "2s ease leftToRight2 both";
+  document.getElementById("backgroundRight2").style["animation"] =
+    "2s ease rightToLeft2 both";
+  document.getElementById("backgroundLeft").style["animation-delay"] = "1s";
+  document.getElementById("backgroundRight").style["animation-delay"] = "1s";
+  document.getElementById("backgroundLeft2").style["animation-delay"] = "1s";
+  document.getElementById("backgroundRight2").style["animation-delay"] = "1s";
+  document.getElementById("displayText").style["animation"] =
+    "2s ease scaleIn both";
+  document.getElementById("displayText").style["animation-delay"] = "1s";
+  display3();
+}
